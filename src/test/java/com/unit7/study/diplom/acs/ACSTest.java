6c843eea3e6140179573ce71a2c616e9d1881942
unit7 <vialurt@gmail.com>
2015-05-02 15:03:22 +0600

    test optimization and correction

--- src/test/java/com/unit7/study/diplom/acs/ACSTest.java
+++ src/test/java/com/unit7/study/diplom/acs/ACSTest.java
@@ -14,6 +14,7 @@ import java.util.SortedSet;
 import java.util.TreeSet;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -37,41 +38,51 @@ public class ACSTest {
         final long k = m;                               // размер второй обучающей выборки
         final long rem = n - m - k;                     // размер тестовой выборки
         
-        logger.debug("Selection count: {}, m: {}, k: {}, testing selection: {}", n, m, k, rem);
+        logger.info("Selection count: {}, m: {}, k: {}, testing selection: {}", n, m, k, rem);
         
         // множество встреченных символов
         final SortedSet<Long> firstSet = new TreeSet<>();
         
         for (int i = 0; i < m; ++i) {
-            final int val = g.next().intValue();
+            final int val = i;//g.next().intValue();
             // make all numbers positive
             firstSet.add((long) val - (long) Integer.MIN_VALUE);
         }
         
         logger.debug("First set after selection: {}", firstSet);
-        logger.debug("Size: {}", firstSet.size());
+        logger.info("First set size: {}", firstSet.size());
         
         // среднее растояние между символами
-        int r = findDistance(firstSet);
+        double r = findDistance(firstSet);
         
-        logger.debug("Firstly finded distance: {}", r);
+        logger.info("Firstly finded distance: {}", r);
         logger.debug("Process first set...");
         
         // should b1 > r
         
-        while (Math.abs(firstSet.first()) <= r) {
+        while (firstSet.first() <= r) {
             logger.debug("Remove first element: {}", firstSet.first());
             firstSet.remove(firstSet.first());
         }
         
         logger.debug("After first condition preparing: {}", firstSet);
+        logger.info("After first condition preparing set size: {}", firstSet.size());
+        
+        // should b[last] < S - R
+        while (firstSet.last() >= (g.power() - r)) {
+            logger.debug("Remove last element: {}", firstSet.last());
+            firstSet.remove(firstSet.last());
+        }
+        
+        logger.debug("After third condition preparing: {}", firstSet);
+        logger.info("After third condition preparing set size: {}", firstSet.size());
         
         // прорежаем выборки и находим новое среднее растояние между символами
-        final int newDist = normalizeSet(firstSet, r);
+        final double newDist = normalizeSet(firstSet, r);
         
-        logger.debug("Newly finded distance: {}", newDist);
+        logger.info("Newly finded distance: {}", newDist);
         logger.debug("After full first set normalization: {}", firstSet);
-        logger.debug("Size: {}", firstSet.size());
+        logger.info("After full first set normalization size: {}", firstSet.size());
         
         // stage 2
         
@@ -107,22 +118,26 @@ public class ACSTest {
             }
         }
         
-        logger.debug("Result frequency: {}", freq);
+        logger.info("Result frequency: {}", freq);
         
         // вероятность выпадания символа из первых двух выборок
         
         final double p = 2.0 * firstSet.size() * distances.size() / g.power();
         
-        logger.debug("Probablity: {}", p);
+        logger.info("Probablity: {}", p);
         
         final double np = rem * p;
         final double np2 = rem * (1 - p);
         
-        // проверяем про критерию хи квадрат
+        // проверяем по критерию хи квадрат
         
         final double x2 = (freq - np) * (freq - np) / np + (rem - freq - np2) * (rem - freq - np2) / np2;
         
-        logger.debug("x2: {}", x2);
+        logger.info("x2: {}", x2);
+        
+        final double hi2 = 0.01579; // 0.9
+        
+        Assert.assertTrue("", hi2 >= (x2 - 0.000001));
     }
     
     private long findMinDist(SortedSet<Long> set, long val) {
@@ -149,9 +164,9 @@ public class ACSTest {
     /**
      * Проредить выборку для выполнения условий
      */
-    private int normalizeSet(SortedSet<Long> set, int r) {
-        int r2 = 2 * r;
-        int newDist = r;
+    private double normalizeSet(SortedSet<Long> set, double r) {
+        double r2 = 2 * r;
+        double newDist = r;
         while (true) {
             boolean finished = true;
             final Iterator<Long> it = set.iterator();
@@ -160,14 +175,11 @@ public class ACSTest {
                 long cur = it.next();
                 int dist = (int) (cur - prev);
                 if (dist >= r2) {
-                    removeWithMinDist(set);
-                    
-                    // TODO новое растояние можно искать за O(1) зная какой символ удалили - в середине или с края
-                    
-                    newDist = findDistance(set);
+                    newDist = removeWithMinDist(set, newDist, dist);
                     r2 = newDist * 2;
                     logger.debug("During normalizing newly distance finded: {}", newDist);
                     finished = false;
+                    break;
                 }
                 
                 // TODO после удаления символа можно продолжить считать с того же места либо откатиться назад, но на
@@ -184,39 +196,112 @@ public class ACSTest {
     
     /**
      * Удалить из множества элемент с минимальной дистанцией до другого
-     * TODO модифицировать на удаление любого элемента без изменения конечного растояния
+     * TODO добавить возможность удаления только последнего элемента наряду с первым
      */
-    private void removeWithMinDist(SortedSet<Long> set) {
-        logger.debug("Remove element from set");
+    private double removeWithMinDist(SortedSet<Long> set, double current, int required) {
+        logger.debug("Remove element from set, current dist: {}, required 2R - 1: {}", current, required);
         
         Preconditions.checkArgument(!set.isEmpty(), "set should be not empty");
         
-        int minDist = Integer.MAX_VALUE;
-        final Iterator<Long> it = set.iterator();
-        Long e = it.next();
-        long prev = e;
-        while (it.hasNext()) {
-            long cur = it.next();
-            int dist = (int) (cur - prev);
-            if (dist < minDist) {
-                minDist = dist;
-                if (it.hasNext()) {
-                    e = cur;
-                } else {
-                    e = prev;
+        // можно попробовать удалять только первый элемент пока не достигнем предела
+        // должно быть b1 > R
+        double newDistance = current;//tryRemoveFirst(set, current, required);
+//        double doubledDistance = newDistance * 2;
+        
+//        if (required < doubledDistance)
+//            return newDistance;
+        
+        if (set.size() < 3)
+            throw new OperationImpossibleException("set size should be >= 3. current: " + set.size());
+        
+        do {
+            final Iterator<Long> it = set.iterator();
+            long pprev = it.next();
+            long prev = it.next();
+            
+            double calced = distanceSum / (set.size() - 2);
+            double doubled = calced * 2;
+            
+            boolean removed = false;
+            
+            while (it.hasNext()) {
+                long cur = it.next();
+                long dist = cur - pprev;
+                
+                // условие выполняется после удаления среднего элемента
+                if (dist < doubled) {
+                    set.remove(prev);
+                    removed = true;
+                    break;
                 }
+                
+                pprev = prev;
+                prev = cur;
             }
-        }
+            
+            logger.debug("Remove element: {}, newDistance: {}", prev, calced);
+            
+            if (required < doubled) {
+                newDistance = calced;
+                break;
+            }
+            
+            if (!removed) {
+                throw new OperationImpossibleException("Operation impossible with this params");
+            }
+            
+        } while(true);
         
-        logger.debug("Element to remove from set: {}", e);
+        return newDistance;
+    }
+    
+    /**
+     * Попробовать удалить первый элемент множества, не нарушив условия множества: первый элемент должен быть больше R
+     * @param set множество
+     * @param current текущее среднее растояние
+     * @param requiredMin требуемое минимальное растояние (required < 2R)
+     * @return новое среднее растояние после удаления элемента
+     * 
+     * Процедура некорректна
+     */
+    @Deprecated
+    private double tryRemoveFirst(SortedSet<Long> set, double current, int requiredMin) {
+        double calced = current;
         
-        set.remove(e);
+        do {
+            if (set.size() < 1)
+                throw new OperationImpossibleException("Set size should be >= 2. current: " + set.size());
+            
+            final Iterator<Long> it = set.iterator();
+            final long e = it.next();
+            final long sec = it.next();
+            
+            double newDist = distanceSum / (set.size() - 2);
+            double doubledDist = newDist * 2;
+            
+            if (sec <= newDist) {
+                break;
+            }
+            
+            calced = newDist;
+            set.remove(e);
+            
+            logger.debug("Condition success. Removed element: {}, newDistance: {}, newNext: {}", e, newDist, sec);
+            
+            if (requiredMin < doubledDist) {
+                break;
+            }
+        } while(true);
+        
+        logger.debug("Newly calculated distance: {}", calced);
+        
+        return calced;
     }
     
     /**
      * Найти среднее растояние между символами в множестве
      */
-    private int findDistance(SortedSet<Long> set) {
+    private double findDistance(SortedSet<Long> set) {
         long result = 0;
         
         final Iterator<Long> it = set.iterator();
@@ -228,13 +313,15 @@ public class ACSTest {
             prev = cur;
         }
         
-        return (int) (result / (set.size() - 1));
+        distanceSum = result;
+        
+        return (double) result / (set.size() - 1);
     }
     
     @After
     public void destroy() {
         final long endTime = System.currentTimeMillis() - startTime;
-        logger.debug("Execution time: {} sec.", ((double) endTime) / 1000);
+        logger.info("Execution time: {} sec.", ((double) endTime) / 1000);
     }
     
     @Before
@@ -247,4 +334,32 @@ public class ACSTest {
     private long startTime;
     
     private Generator<Integer> g;
+    
+    private long distanceSum;
+}
+
+class OperationImpossibleException extends RuntimeException {
+
+    private static final long serialVersionUID = 2978838653696279982L;
+
+    public OperationImpossibleException() {
+        super();
+    }
+
+    public OperationImpossibleException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public OperationImpossibleException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public OperationImpossibleException(String message) {
+        super(message);
+    }
+
+    public OperationImpossibleException(Throwable cause) {
+        super(cause);
+    }
+    
 }
